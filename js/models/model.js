9a5cd52d2f5b88966b4c3cb9920ce767b39020dc
Avery Pratt <apratt91@gmail.com>
2016-11-14 11:47:22 -0800

    added submission form

--- js/models/model.js
+++ js/models/model.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('model works');
+var obj = {cuisine: [new Opt('chinese'), new Opt('korean'), new Opt('indian')]};
+
+function Opt(cuisine){
+  this.cuisine = cuisine;
+  this.toHTML = function(){
+    var template = $('#submissionFormTemplate').html();
+    var compile = Handlebars.compile(template);
+    return compile(this);
+  };
+};
+
+obj.cuisine.forEach(function(cuisineOption){
+  $('.cuisineSelector').append(cuisineOption.toHTML());
+});
