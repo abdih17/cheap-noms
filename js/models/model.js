1c9bafd130bfe04e9f6cd3f43278d20018f35952
Peter Kim <peterkim23@gmail.com>
2016-11-14 13:23:19 -0800

    google maps merge

--- js/models/model.js
+++ js/models/model.js
@@ -18,18 +18,3 @@
   module.restaurantObj = restaurantObj;
 
 })(window);
-
-var obj = {cuisine: [new Opt('chinese'), new Opt('korean'), new Opt('indian')]};
-
-function Opt(cuisine){
-  this.cuisine = cuisine;
-  this.toHTML = function(){
-    var template = $('#submissionFormTemplate').html();
-    var compile = Handlebars.compile(template);
-    return compile(this);
-  };
-};
-
-obj.cuisine.forEach(function(cuisineOption){
-  $('.cuisineSelector').append(cuisineOption.toHTML());
-});
