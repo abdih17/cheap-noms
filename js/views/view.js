805ed649b5abae1aee4483bc6d490975b4367557
Hawa Abdi <abdih17@hotmail.com>
2016-11-17 09:20:36 -0800

    Lastest styling, sending it up.

--- js/views/view.js
+++ js/views/view.js
@@ -81,4 +81,13 @@ function initMap(locationIndex){
   }
 }
 
+function myFunction() {
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+}
+
 initMap();
